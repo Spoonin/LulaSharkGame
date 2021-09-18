de12ef24173b2a3eb8c05e4394575377b769961e
Denis Lozhkin <dlozhkin@xbsoftware.com>
2021-09-18 15:26:30 +0300

    add interactivity with pointer

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 
-export default {
+export const config = {
     type: Phaser.AUTO,
     parent: 'game',
     backgroundColor: '#33A5E7',
@@ -8,6 +8,19 @@ export default {
         width: 800,
         height: 600,
         mode: Phaser.Scale.FIT,
-        auto_center: Phaser.Scale.CENTER_BOTH,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    loader: {
+        baseURL: 'https://labs.phaser.io',
+        crossOrigin: 'anonymous',
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            debugShowBody: true,
+        },
     },
 }
+
+export default config
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,6 +1,14 @@
-import Phaser from 'phaser'
+import Phaser, { GameObjects } from 'phaser'
+
+const SPEED = 100
+const ROTATION_SPEED = 1 * Math.PI
+const ROTATION_SPEED_DEGREES = Phaser.Math.RadToDeg(ROTATION_SPEED)
+const TOLERANCE = 0.02 * ROTATION_SPEED
+const velocityFromRotation = Phaser.Physics.Arcade.ArcadePhysics.prototype.velocityFromRotation
 
 export default class Demo extends Phaser.Scene {
+    #ship: Phaser.Types.Physics.Arcade.ImageWithDynamicBody = {} as any
+    #emitter: GameObjects.Particles.ParticleEmitter = {} as any
     constructor() {
         const config = {
             type: Phaser.AUTO,
@@ -16,30 +24,47 @@ export default class Demo extends Phaser.Scene {
         super(config)
     }
 
-    preload() {
-        this.load.setBaseURL('http://labs.phaser.io')
-
+    preload(): void {
         this.load.image('sky', 'assets/skies/space3.png')
         this.load.image('logo', 'assets/sprites/phaser3-logo.png')
         this.load.image('red', 'assets/particles/red.png')
+        this.load.image('ship', 'assets/sprites/thrust_ship.png')
     }
 
-    create() {
+    create(): void {
         this.add.image(400, 300, 'sky')
+        this.#ship = this.physics.add.image(200, 150, 'ship')
         const particles = this.add.particles('red')
 
-        const emitter = particles.createEmitter({
+        this.#emitter = particles.createEmitter({
             speed: 100,
             scale: { start: 1, end: 0 },
             blendMode: 'ADD',
         })
+    }
+    #pointerMove(pointer: Phaser.Input.Pointer & GameObjects.GameObject): void {
+        if (!pointer.manager.isOver) return
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+        // Also see alternative method in
+        // <https://codepen.io/samme/pen/gOpPLLx>
 
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
+        this.physics.accelerateToObject(this.#ship, pointer, 600, 3000, 3000)
 
-        emitter.startFollow(logo)
+        const angleToPointer = Phaser.Math.Angle.Between(this.#ship.x, this.#ship.y, pointer.worldX, pointer.worldY)
+        const angleDelta = Phaser.Math.Angle.Wrap(angleToPointer - this.#ship.rotation)
+
+        if (Phaser.Math.Within(angleDelta, 0, TOLERANCE)) {
+            this.#ship.rotation = angleToPointer
+            this.#ship.setAngularVelocity(0)
+        } else {
+            this.#ship.setAngularVelocity(Math.sign(angleDelta) * ROTATION_SPEED_DEGREES)
+        }
+
+        this.#emitter.startFollow(pointer)
+    }
+    update(): void {
+        const pointer = this.input.activePointer as unknown as Phaser.Input.Pointer & GameObjects.GameObject
+        this.#pointerMove(pointer)
+        velocityFromRotation(this.#ship.rotation, SPEED, this.#ship.body.velocity)
     }
 }
